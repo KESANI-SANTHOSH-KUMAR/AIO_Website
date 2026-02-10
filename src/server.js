780b4c3e7aad1ca9cf8b4623735d3aa20bf3411a
KESANI SANTHOSH KUMAR <kesanisanthoshkumar3@gmail.com>
2026-02-10 17:34:10 +0530

    Change database password in server.js
    Updated database password for security reasons.

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'SUS77#ta',
+  password: 'your password',
   database: 'mywebsite'
 });
 
